bb5521b20696f65208d49dd181a4e735f9f7d855
jim <jimq991@gmail.com>
2019-08-05 22:20:39 -0400

    added model & postgres

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    # Property of the admin web app
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
