fc34c8720b2503f9becde37c44655707670fba06
jim <jimmyqiu91@gmail.com>
2019-08-24 00:40:13 -0400

    change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'Lenovo003352',
+        'PASSWORD': 'hello123',
         'HOST': '127.0.0.1',
         'PORT': '5432'
 
